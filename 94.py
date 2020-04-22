f193b3f82987c87df7627e5e19188873def3c789
yachialice <4003041997@163.com>
2020-04-22 16:55:41 +0800

    Update 94.py

--- 94.py
+++ 94.py
@@ -1,5 +1,10 @@
 
 #94. Binary Tree Inorder Traversal
+"""
+递归方法注意在递归函数前加self。迭代方法思路为令root的left child node一直入栈，并令left child node作为新的root。
+当root为空时，说明遍历到最左节点，此时出栈一个node，加入output，令node.right作为新的root继续遍历。
+注意if not stack ...不能加载if not root后，否则可能会在最初的根结点提前结束循环。
+"""
 
 
 # Definition for a binary tree node.
